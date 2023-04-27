1bdfeddea33891fb1a489fa76f1854f39cf0d53a
ChisanuchaK <97660202+ChisanuchaK@users.noreply.github.com>
2023-04-27 11:35:27 +0700

    first commit

---
+++ curency-exchange-service/src/test/java/com/example/curencyexchangeservice/CurencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
